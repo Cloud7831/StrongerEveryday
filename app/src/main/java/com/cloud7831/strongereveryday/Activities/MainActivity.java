79f5d0196ed7eb776bc24506c0c1b21003afb6e3
Jayden <JaydenMcPhedrain@gmail.com>
2019-05-15 22:35:43 -0700

    Changed the Activities to Fragments, because that's what I meant to do.

--- app/src/main/java/com/cloud7831/strongereveryday/Activities/MainActivity.java
+++ app/src/main/java/com/cloud7831/strongereveryday/Activities/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    //TODO: set a sliding tabs thing at the top of the screen. Viewpager?
+
 
     }
 
