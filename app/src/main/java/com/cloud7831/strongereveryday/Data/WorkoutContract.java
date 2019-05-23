0b4199b228f969c73e127aea800c42259d90927e
Jayden <JaydenMcPhedrain@gmail.com>
2019-05-22 23:31:20 -0700

    Basework of setting up content URIs for the workouts.

---
+++ app/src/main/java/com/cloud7831/strongereveryday/Data/WorkoutContract.java
@@ -0,0 +1,13 @@
+package com.cloud7831.strongereveryday.Data;
+
+import android.net.Uri;
+
+public final class WorkoutContract {
+
+    public static final String CONTENT_AUTHORITY = "com.cloud7831.strongereveryday";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_WORKOUT = "workout";
+    public static final Uri WORKOUT_CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_WORKOUT);
+
+
+}
