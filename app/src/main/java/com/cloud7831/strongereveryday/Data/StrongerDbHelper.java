f4fc2be2eff718fbaec6417f5a17272be8ddc230
Jayden <JaydenMcPhedrain@gmail.com>
2019-07-23 23:18:06 -0700

    Setup basic table construction for creating a database for user workouts.

---
+++ app/src/main/java/com/cloud7831/strongereveryday/Data/StrongerDbHelper.java
@@ -0,0 +1,38 @@
+package com.cloud7831.strongereveryday.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.cloud7831.strongereveryday.Data.WorkoutContract.WorkoutEntry;
+
+public class StrongerDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "StrongerEveryday.db";
+
+    public StrongerDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        //TODO: I probably need a total of two tables.
+        //TODO: one for the workouts, and one for the exercises.
+
+        //TODO: make a table for storing the MyFit scores, plus weight, etc.
+
+
+        String SQL_CREATE_WORKOUTS_TABLE = "CREATE TABLE " + WorkoutEntry.TABLE_NAME + "{"
+                                                           + WorkoutEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                                           + WorkoutEntry.WORKOUT_NAME + "TEXT NOT NULL, "
+                                                           + WorkoutEntry.WORKOUT_LAST_COMPLETED + "INTEGER NOT NULL DEFAULT -1);"; // Last completed = -1 days means it has never been completed.
+        db.execSQL(SQL_CREATE_WORKOUTS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1){
+
+    }
+
+}
--- app/src/main/java/com/cloud7831/strongereveryday/Data/WorkoutContract.java
+++ app/src/main/java/com/cloud7831/strongereveryday/Data/WorkoutContract.java
@@ -1,13 +1,38 @@
 package com.cloud7831.strongereveryday.Data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public final class WorkoutContract {
 
     public static final String CONTENT_AUTHORITY = "com.cloud7831.strongereveryday";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-    public static final String PATH_WORKOUT = "workout";
-    public static final Uri WORKOUT_CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_WORKOUT);
+    public static final String PATH_WORKOUTS = "workouts";
+
+    public static abstract class WorkoutEntry implements BaseColumns {
+        public static final Uri WORKOUT_CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_WORKOUTS);
+
+        /**
+         * The MIME type of the {@link #WORKOUT_CONTENT_URI} for a list of workouts.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WORKOUTS;
+
+        /**
+         * The MIME type of the {@link #WORKOUT_CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WORKOUTS;
+
+
+        public final static String TABLE_NAME = "workouts";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String WORKOUT_NAME = "name";
+        public final static String WORKOUT_LAST_COMPLETED = "last completed";
+
+    }
 
     public enum WorkoutCardType{
         EXERCISE,
